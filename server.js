fddb18a0dde80afb9e938fa3be33620599f2e2c6
Rithinrajkp <30979436+Rithinrajkp@users.noreply.github.com>
2017-08-13 23:43:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req, res){
-   res.send('/article-one will be sent and served here'); 
+   res.send('Article-one requested will be served here'); 
 });
 
 app.get('/article-two',function(req, res){
-   res.send('/article-two will be sent and served here'); 
+   res.send('Article-two requested will be served here'); 
 });
 
 app.get('/article-three',function(req, res){
-   res.send('/article-three will be sent and served here'); 
+   res.send('Article-three requested will be served here'); 
 });
 
 app.get('/ui/style.css', function (req, res) {
