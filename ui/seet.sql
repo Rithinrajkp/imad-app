6e60c663bb31dae0ccd262041b15b3a851f7cf69
Rithinrajkp <30979436+Rithinrajkp@users.noreply.github.com>
2017-09-13 12:35:59 +0530

    Update seet.sql

--- ui/seet.sql
+++ ui/seet.sql
@@ -1,30 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Sep 09, 2017 at 10:52 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `seet`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mst_admin`
---
 
 CREATE TABLE `mst_admin` (
   `id` int(11) NOT NULL,
